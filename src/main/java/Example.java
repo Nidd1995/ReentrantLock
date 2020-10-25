e5a69a051c6aa0d0d702fadf4e99eda8f11fb0fe
nidhi <tech.nidhiverma@gmail.com>
2020-10-25 17:21:20 +0530

    Implementing Locks

---
+++ src/main/java/Example.java
@@ -0,0 +1,65 @@
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Example {
+
+    private final ReentrantLock lock = new ReentrantLock();
+    private int count = 0;
+
+    public static void main(String[] args) {
+        final Example counter = new Example();
+        Thread t1 = new Thread() {
+            @Override
+            public void run() {
+                while (counter.getCount() < 6) {
+
+                    try {
+                        Thread.sleep(100);
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+
+        };
+
+        Thread t2 = new Thread() {
+            @Override
+            public void run() {
+                while (counter.getCount() < 6) {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+
+        };
+
+        t1.start();
+        t2.start();
+    }
+
+    //Implicit Locking using synchronized keyword
+//    public synchronized int getCount(){
+//        return count++;
+//    }
+
+    //locking using Reentrant Lock
+    public int getCount() {
+        lock.lock();
+
+        try {
+            System.out.println(Thread.currentThread().getName() + count);
+            return count++;
+        } finally {
+            lock.unlock();
+        }
+
+    }
+
+
+}
